3a448c86594c7ce63b35964bbb6edb884d7077cd
tushar valani <Tusharvalani20@gmail.com>
2022-05-25 15:16:19 +0530

    Added

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+   
+    <div className="Header">
+      <h1>REACT JS</h1>
+     
+     <div>
+     <h2>TODO-LIST APP</h2>
+     </div>
+     </div>
+  )
+}
+
+export default Header
